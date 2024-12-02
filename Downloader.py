1e7867cefbc161125a430b61ba35052df7d08bc9
Patrick <patrickbruusd@gmail.com>
2024-12-02 11:29:54 +0100

    Refactored downloader, changed some names and split up the code for easier reading and simplified the code, and better for testing

--- Downloader.py
+++ Downloader.py
@@ -6,39 +6,42 @@ class Downloader(object):
 
     #uses a url link and a destination to download a file. Optionally one can use an alt url if applicaple
     #Returns success is file got downlaoded
-    def download(self,url : str, destination_path : str, alt_url : Optional[str] = None) -> bool:
+    def download_handling(self, url : str, destination_path : str, alt_url : Optional[str] = None) -> bool:
 
-        success = True
+        success = False
         if not url and not alt_url:
-            return False
+            return success
         #Tries downloading with the main url
+        fileDownloaded, response = self.download(url)
+        
+        #If it fails to download try the alternative url
+        if not fileDownloaded and alt_url:
+           fileDownloaded, response = self.download(alt_url)
+        
+        #Sace file to the distination if the download was success
+        if fileDownloaded:
+            success = self.save_to_file(destination_path,response)
+       
+        return success
+
+    def save_to_file(self, destination_path : str, response):
+         with open(destination_path, "wb") as file:
+            try:
+                file.write(response.content)
+                return True
+            except:
+                return False
+
+    def download(self, url : str):
         try:
             response = requests.get(url,stream = True, timeout=30)
             #Checks if the response was a pdf file
-            if not "application/pdf" in response.headers.get("content-type"):
-                raise Exception("Not pdf type")
+            if "application/pdf" in response.headers.get("content-type"):
+                return True, response
+            return False, None
         except:
-            success = False
-        
-        #If it fails to download try the alternative url
-        if not success:
-            try:
-                response = requests.get(alt_url, stream = True, timeout=30)
-                #Checks if the response was a pdf file
-                if not "application/pdf" in response.headers.get("content-type"):
-                    raise Exception("Not pdf type")
+            return False, None 
 
-                success = True
-            except:
-                return False
-       
-        #Sace file to the distination
-        with open(destination_path, "wb") as file:
-            try:
-                file.write(response.content)
-            except:
-                return False
-        return success
 if __name__ == "__main__":
     downloader = Downloader()
-    downloader.download("http://arpeissig.at/wp-content/uploads/2016/02/D7_NHB_ARP_Final_2.pdf", "test.pdf")
+    downloader.download_handling("http://arpeissig.at/wp-content/uploads/2016/02/D7_NHB_ARP_Final_2.pdf", "test.pdf")
