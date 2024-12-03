8389b66adaceefdaf9aabb093497c3b9b8498c90
Patrick <patrickbruusd@gmail.com>
2024-12-03 14:40:17 +0100

    fixed some bugs the metadata is taking the allrady downloaded and trying to download it again, and fixed a bug where the list of downloaded is not threadsafe, test if folder exist moved to only check i once instead of each time. Refactoring and spiltting up code to make it easier to test

--- Polar_File_Handler.py
+++ Polar_File_Handler.py
@@ -9,53 +9,62 @@ from xlsxwriter import Workbook
 
 #Class for handling a file with download links
 class FileHandler(object):
-   
+    
     #Creates an instance of the filehandler with a dictionary of successfull downloads
-    def __init__(self,number_of_threads : Optional[int] = 10) -> None:
+    def __init__(self, url_file : str, meta_file : str, destination : str, number_of_threads : Optional[int] = 10) -> None:
         self.number_of_threads = number_of_threads
+        self.url_file = url_file
+        self.meta_file = meta_file
+        self.destination = destination
+
+        self.download_status_list = []
+        self.list_lock = threading.RLock()
+        self.ID = "BRnum"
+        
+    def add_download_status(self, item):
+        with self.list_lock:  # Lock can be acquired multiple times by the same thread
+            self.download_status_list.append(item)
+
 
     #Function that starts a download instance using the downloader class. Used in threads
-    def download_thread(self,queue:Queue) -> None:
+    def thread_downloader(self, queue:Queue) -> None:
         while not queue.empty():
             
-            link, destination, name, alt_link, finished_dict = queue.get()
+            link, destination, name, alt_link = queue.get()
+
             downloader = Downloader()
-            Path(destination).mkdir(exist_ok=True)
+            downloaded = downloader.download_handling(url=link,destination_path=os.path.join(destination, name+".pdf"), alt_url=alt_link)
 
             #Dictionaries are not necesarily thread safe but appending to it is so this is fine. If more complicated tasks where needed you would use a mutex lock etc
-            finished_dict["BRnum"].append(name)
-            downloaded = downloader.download(url=link,destination_path=os.path.join(destination, name+".pdf"), alt_url=alt_link)
+            
             if downloaded:
-                finished_dict["pdf_downloaded"].append("yes")
+                state = "yes"
             else:
-                finished_dict["pdf_downloaded"].append("no")
+                state = "no"
+            self.add_download_status((name, state))
             queue.task_done()
 
-    #Starts downlaoding files from urls listed in url_file which will be placed in the destination, and reported in the meta file
-    def start_download(self, url_file : str,meta_file : str,destination : str) -> None:
 
-        file_data = pl.read_excel(source=url_file, columns= ["BRnum", "Pdf_URL", "Report Html Address"])
-        
-        #We index after the BRnums for now
-        ID = "BRnum"
 
+    def import_data_files(self):
+        file_data = pl.read_excel(source=self.url_file, columns= ["BRnum", "Pdf_URL", "Report Html Address"])
+       
         #Initiates empty dataframe
-        report_data = pl.DataFrame()
-        #Tries reading the files listed as not downloaded if it fails it will make a new meta file that fullfills the structure
-        try:
-            report_data = pl.read_excel(meta_file, columns = [ID,"pdf_downloaded"])
-            report_data=report_data.filter(pl.col("pdf_downloaded") == "yes")
+        meta_data = pl.DataFrame()
+        #Tries reading the files listed as not downloaded
+        if os.path.exists(self.meta_file):
+                meta_data = pl.read_excel(self.meta_file, columns = [self.ID,"pdf_downloaded"])
+                meta_data=meta_data.filter(pl.col("pdf_downloaded") == "no")
             #Sort out files that are downloaded
-            file_data = file_data.join(report_data,on=ID, how="anti")
-        except:
-            print("New meta data file will be created")
+                file_data = file_data.join(meta_data,on=self.ID, how="anti")
+        
+        return file_data, meta_data
 
+
+    def thread_handler(self, file_data): 
         queue = Queue()
 
-        #Creates a dictionary of downloads
-        finished_dict = {ID:[],"pdf_downloaded":[]}
-
-        #counter to only download 10 files
+        #counter to only download 20 files
         j = 0
         #We thru each br number and starts a download
         for row in file_data.rows(named=True):
@@ -63,25 +72,31 @@ class FileHandler(object):
                 break
             alt_link = row["Report Html Address"]
             link = row["Pdf_URL"]
-            index = row[ID]            
+            index = row[self.ID]            
             #Creates a new thread and adds them to the list so that we can make sure all downloads are done before exiting
-            queue.put([link,destination,index,alt_link,finished_dict])
+            queue.put([link,self.destination,index,alt_link])
             j += 1
         #Makes sure each thread is done
         for i in range(self.number_of_threads):
-            thread = threading.Thread(target=self.download_thread, args = (queue,))
+            thread = threading.Thread(target=self.thread_downloader, args = (queue,))
             thread.start()
 
         queue.join()
-        #Creates a dataframe from the dictionary of downloads
-        finished_data_frame = pl.from_dict(finished_dict)
 
-        if not report_data.is_empty():
-            finished_data_frame = pl.concat([finished_data_frame,report_data],rechunk = True)
-        with Workbook(meta_file) as file:
-            finished_data_frame.write_excel(workbook = file)
+
+    #Starts downlaoding files from urls listed in url_file which will be placed in the destination, and reported in the meta file
+    def handler(self) -> None:
+        #Tests if Path exist and if not creates directory
+        Path(self.destination).mkdir(exist_ok=True)
+
+        file_data, meta_data = self.import_data_files()
+        if not file_data.is_empty():    
+            self.thread_handler(file_data)
+            #Creates a dataframe from the dictionary of downloads
+            finished_data_frame = pl.DataFrame(self.download_status_list, schema=[self.ID, "pdf_downloaded"])
+
+            if not meta_data.is_empty():
+                finished_data_frame = pl.concat([finished_data_frame,meta_data],rechunk = True)
+            with Workbook(self.meta_file) as file:
+                finished_data_frame.write_excel(workbook = file)
         
-
-if __name__ == "__main__":
-    file_handler = FileHandler()
-    file_handler.start_download(os.path.join("customer_data","GRI_2017_2020.xlsx"),os.path.join("customer_data","Metadata2017_2020.xlsx"),"files")
