7c8e04634cce31f4fddf32ac08d898d7c78c71dc
Patrick <patrickbruusd@gmail.com>
2024-12-04 09:13:47 +0100

    rearranged project

--- src/Controller.py
+++ src/Controller.py
@@ -8,12 +8,18 @@ class Controller(object):
 
     #Initiates the controller class with default values
     def __init__(self) -> None:
-        self.url_file_name = os.path.join("customer_data","GRI_2017_2020.xlsx")
-        self.report_file_name = os.path.join("customer_data","Metadata2017_2020.xlsx")
-        self.destination = "files"
+        parent_folder = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+        # Create a path for the "files" folder in the parent folder
+        files_folder = os.path.join(parent_folder, "files")
+        data_folder = os.path.join(parent_folder, "customer_data")
+        self.url_file_name = os.path.join(data_folder,"GRI_2017_2020.xlsx")
+        self.report_file_name = os.path.join(data_folder,"Metadata2017_2020.xlsx")
+        self.destination = files_folder
     
     #Runs the filehandler
     def run(self):
+        
         file_handler = FileHandler(self.url_file_name,self.report_file_name,self.destination,1)
         file_handler.handler()
 
